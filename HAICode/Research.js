0e981cf85a4661ba8607825f7401ae8e465b691c
ho-sick99 <83945722+JunoJuno99@users.noreply.github.com>
2023-11-24 13:20:40 +0900

    feature #37 프로젝트 정보 저장 함수(createResearchComponents) 생성

--- HAICode/Research.js
+++ HAICode/Research.js
@@ -14,9 +14,9 @@ function isUndefined(str) {
  * createResearchComponents 호출
  * @param containerId 해당 결과를 저장할 container(Tag)의 ID
  */
-function loadPeopleJson(containerId) {
+function loadResearchJson(containerId) {
     readJson("../Data/research.json", (json) => {
-        peopleData = json["data"];
+        projects = json["data"].projects;
         createResearchComponents(containerId);
     });
 }
@@ -29,61 +29,25 @@ function createResearchComponents(containerId) {
     let components = "";
     let isLeftImage = true;
     let contents = "";
-    Object.keys(peopleData).forEach((person) => {
-        contents = "";
-        peopleData[person].forEach((profile) => {
-            let name = profile["name"];
-            let degree = profile["degree"];
-            let major = profile["major"];
-            let affiliation = "";
-            let keyword = profile["keyword"];
-            let imagePath = profile["imagePath"];
-            let email = profile["email"];
-            let link = profile["link"];
-            let office = "";
-            let laboratory = "";
-            let cv = "";
-            // Professor
-            if (person === "Professor") {
-                affiliation = profile["affiliation"];
-                office = profile["office"];
-                laboratory = profile["laboratory"];
-                cv = profile["cv"];
-            }
-            contents += profileWithImage(
-                name,
-                affiliation,
-                degree,
-                major,
-                keyword,
-                email,
-                imagePath,
-                link,
-                office,
-                laboratory,
-                cv,
-                isLeftImage,
-                false
-            );
-            isLeftImage = !isLeftImage;
-        });
-        components += customTitleWithContent(
-            person,
-            contents,
-            "padding-top: 4rem; border-bottom: solid 2px #e2e2e2",
+    
+    projects.forEach((project) => {
+        let imagePath = project["imagePath"];
+        let projectName = project["projectName"];
+        let description = project["description"];
+        let projectRepository = project["projectRepository"];
+        let relatedPublications = project["relatedPublications"];
+        contents += researchWithImage(
+            imagePath,
+            projectName,
+            description,
+            projectRepository,
+            relatedPublications,
+            isLeftImage,
             false
         );
+        isLeftImage = !isLeftImage;
     });
 
-    if (!isUndefined(peopleData["haiIntroduction"])) {
-        components += titleAndDescriptionWithImage(
-            peopleData["title"],
-            peopleData["haiIntroduction"],
-            peopleData["groupImagePath"],
-            false
-        );
-    }
-
     let container = document.getElementById(containerId);
-    container.innerHTML = components;
+    container.innerHTML = contents;
 }
